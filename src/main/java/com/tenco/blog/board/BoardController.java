4d1611333e5c2f8283c447c825be40d5942c98df
suuu1021 <whs5758@naver.com>
2025-06-20 09:42:29 +0900

    기본 패키지 변경 도메인 중심

---
+++ src/main/java/com/tenco/blog/board/BoardController.java
@@ -0,0 +1,8 @@
+package com.tenco.blog.board;
+
+import org.springframework.stereotype.Controller;
+
+@Controller // IoC 대상 - 싱글톤 패턴으로 관리 됨
+public class BoardController {
+
+}
---
+++ src/main/java/com/tenco/blog/board/BoardRequest.java
@@ -0,0 +1,8 @@
+package com.tenco.blog.board;
+
+/**
+ * 클라이언트에게서 넘어온 데이터를
+ * Object로 변화해서 전달하는 DTO 역할을 담당한다.
+ */
+public class BoardRequest {
+}
